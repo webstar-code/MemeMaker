deb6cab78775cec9425c34208b678351c29ee920
webstar-code <bhaveshrc2001@gmail.com>
2020-04-24 08:24:50 +0530

    Fully Working

--- assests/js/index.js
+++ assests/js/index.js
@@ -7,7 +7,9 @@ function addtext() {
     const input = document.createElement('textarea');
     inputdiv.classList.add("dragdiv");
     inputdiv.id = "id";
+    input.setAttribute('placeholder', 'Type here...');
     inputdiv.append(input);
+
     $(".container").append(inputdiv);
     $("textarea").resizable({
         maxHeight: contH,
@@ -20,7 +22,7 @@ function addtext() {
     $('textarea').bind('click', function () {
         $(this).focus();
     });
-    // $('textarea').fitText(0.8);
+    $('textarea').fitText(0.8);
 }
 
 $("#addtext").click(() => {
@@ -29,18 +31,20 @@ $("#addtext").click(() => {
 
 // ======================  Fetch Images =================================
 let row = $(".row");
-getMeme();
+// getMeme();
 
 async function getMeme() {
     const meme = await fetch("https://api.imgflip.com/get_memes");
     const result = await meme.json();
 
-    console.log(result);
+    // console.log(result);
 
     const memes = result.data.memes;
     const column = document.createElement('div');
     column.classList.add("column");
 
+    const containerImg = document.getElementById('container-img');
+    // containerImg.src = memes[Math.floor(Math.random()*100)].url;  // Random Image initially in containers
     memes.forEach(item => {
         const url = item.url;
         const img = document.createElement('img');
@@ -79,8 +83,9 @@ function handleimg(imgs, name) {
     const containerImg = document.getElementById('container-img');
     containerImg.src = imgs.src;
     Imgname = name;
-     contH = $(".container").height();
+    contH = $(".container").height();
     contW = $(".container").width();
+
 }
 
 // ======================= Change color ====================================
@@ -97,19 +102,71 @@ $(".redbtn").click(() => {
 // ===================== Download the image ===================================
 
 
+// let element = $(".container");
+// let ImageData;
+// $("#downloadbtn").on('click', function () {
+//     let getCanvas;
+//     console.log("he;;p");
+//     html2canvas(element, {
+//         onrendered: function (canvas) {
+//             // $("#preview-img").append(canvas);
+//         console.log(element);
+
+//             getCanvas = canvas;
+//             ImageData = getCanvas.toDataURL("image/jpeg");
+//             let newData = ImageData.replace(/^data:image\/jpeg/, "data:application/octet-stream");
+//             $("#downloadbtn").attr("download", "upur.jpeg").attr("href", newData);
+//         }
+//     });
+// });
+
+
+
 let element = $(".container");
 let ImageData;
-$("#downloadbtn").on('click', function () {
-    let getCanvas;
+$("#downloadbtn").on('click', function() {
+let getCanvas;
 
-    html2canvas(element, {
-        onrendered: function (canvas) {
-            // $("#preview-img").append(canvas);
-            getCanvas = canvas;
-            ImageData = getCanvas.toDataURL("image/jpeg", 0.9);
-            let newData = ImageData.replace(/^data:image\/jpeg/, "data:application/octet-stream");
-            $("#downloadbtn").attr("download", `${Imgname}.jpeg`).attr("href", newData);
+    html2canvas(element,{
+        // allowTaint: true, //Worked without this!
+        useCORS: true,
+        onrendered: function(canvas) {
+            $("#downloadbtn").attr("download","pic.jpeg").attr("href",canvas.toDataURL('image/jpeg'));
         }
     });
+
+        // useCORS: true,
+//         onrendered: function(canvas) {
+//             console.log(element);
+//             // $("#preview-img").append(canvas);
+//             getCanvas = canvas;
+//             ImageData = getCanvas.toDataURL("image/jpeg",0.9);
+//  let newData = ImageData.replace(/^data:image\/jpeg/, "data:application/octet-stream");
+//     $("#downloadbtn").attr("download", "ypu-pic.jpeg").attr("href", newData);
+        // }
+    // });
+    // let ImageData = getCanvas.toDataURL("image/png");
+    // console.log(ImageData);
+   
+})
+
+
+// ==================== Display Home page ==========================
+$(".Home-menu").click(() => {
+    // $(".Home-container").show();
+    $("img").css({'opacity': '0.5'});
+    // $(".Home-menu").hide();
+    $(".Home-container").fadeIn();
+
+})
+
+$(".close").click(() => {
+    $(".Home-container").hide();
+    $(".Home-menu").show();
+    $(".Home-container").fadeOut("slow");
+
+
+    $("img").css({'opacity': '1'});
+
 });
 
